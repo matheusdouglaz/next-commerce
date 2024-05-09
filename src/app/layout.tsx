baad4dad2cc28f9ef6b766f8723914c900c301bf
matheusdouglaz <matheustecnologi@outlook.com>
2024-05-09 08:03:30 -0300

    Adicionando Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,22 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
+import Navbar from './components/Navbar';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Next E-commerce',
   description: 'Next E-commerce',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        <main className='bg-slate-700 h-screen p-16'>{children}</main>
+      </body>
     </html>
-  )
+  );
 }
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main>
+    <div>
       <h1>Next E-commerce</h1>
-    </main>
+    </div>
   )
 }

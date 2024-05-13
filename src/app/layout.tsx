d566035d8647fa56015f19aa31fc18639499d0f4
matheusdouglaz <matheustecnologi@outlook.com>
2024-05-13 10:25:57 -0300

    Adicionando autenticação com Clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css';
 import clsx from 'clsx';
 import { Inter } from 'next/font/google';
 import Navbar from './components/Navbar';
+import { ClerkProvider} from '@clerk/nextjs';
+import { ptBR } from "@clerk/localizations";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,12 +14,16 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <ClerkProvider localization={ptBR}>
+      <html lang="en">
       <body className={clsx(inter.className, 'bg-slate-700')}>
         <Navbar />
         <main className=' h-screen p-16'>{children}</main>
       </body>
     </html>
+
+    </ClerkProvider>
+    
   );
 }
 
